7a69c785351c34cbd0735fb91a19fdc3f3f1d31e
Till Hoffmann <tillahoffmann@gmail.com>
2024-05-02 17:14:13 -0400

    Add inference module.

--- avb/__init__.py
+++ avb/__init__.py
@@ -1,11 +1,14 @@
 from .expectation import expect, expect_log_prob
 from . import distributions
+from .infer import elbo_loss, expect_log_joint
 from .nodes import delay, DelayedDistribution
 
 __all__ = [
     "delay",
     "DelayedDistribution",
     "distributions",
+    "elbo_loss",
     "expect",
+    "expect_log_joint",
     "expect_log_prob",
 ]
--- tests/test_infer.py
+++ tests/test_infer.py
@@ -9,7 +9,7 @@ import numpyro
 from numpyro import distributions
 
 
-def model(delay, n, p) -> None:
+def linear_model(n=7, p=2, *, delay) -> None:
     if delay:
         new = avb.DelayedDistribution
     else:
@@ -23,57 +23,46 @@ def model(delay, n, p) -> None:
     numpyro.sample("y", new(PrecisionNormal, y_hat, tau))
 
 
-def test_smoke() -> None:
+def test_delayed_eager_equivalence_and_structure() -> None:
     # First check that, in the eager mode, sampling is exactly as we'd expect from
     # numpyro.
-    n = 50
-    p = 2
 
     samples = []
     for delay in [False, True]:
         with numpyro.handlers.seed(rng_seed=17):
-            trace = numpyro.handlers.trace(model).get_trace(delay, n, p)
+            trace = numpyro.handlers.trace(linear_model).get_trace(delay=delay)
         sample = {name: site["value"] for name, site in trace.items()}
         samples.append(sample)
 
     jax.tree.map(np.testing.assert_allclose, *samples)
 
-    # Validate the structure of the delayed trace.
+    # Get a delayed trace and check its structure.
     with numpyro.handlers.trace() as delayed_trace, avb.delay():
-        numpyro.handlers.trace(model).get_trace(True, n, p)
+        numpyro.handlers.trace(linear_model).get_trace(delay=True)
 
-    assert set(trace) == set(delayed_trace)
+    assert set(delayed_trace) == {"X", "y", "coef", "intercept", "tau"}
     for name, site in delayed_trace.items():
         assert isinstance(site["value"], avb.nodes.DelayedValue), name
         assert isinstance(site["fn"], avb.nodes.DelayedDistribution), name
 
-    # Create the substitution for which we want to evaluate the expected log joint
-    # distribution. Substitutions are either concrete values or variational factors to
-    # be optimized.
-    substitutions = sample | {
-        "tau": distributions.Gamma(4, 3),
-        "coef": PrecisionNormal(jnp.asarray([-0.2, 0.3]), jnp.asarray([0.9, 1.1])),
-        "intercept": PrecisionNormal(0.4, 1.4),
-    }
-    with (
-        numpyro.handlers.trace() as conditioned_trace,
-        avb.delay(),
-        numpyro.handlers.condition(data=substitutions),
-    ):
-        numpyro.handlers.trace(model).get_trace(True, n, p)
 
-    # Evaluate the expected log joitn distribution by iterating over all sites.
-    elp = {}
-    for name, site in conditioned_trace.items():
-        fn: avb.DelayedDistribution = site["fn"]
-        value = avb.expect_log_prob(
-            fn.cls,
-            site["value"],
-            *fn.args,
-            **fn.kwargs,
-        ).sum()
-        assert jnp.isfinite(value).all()
-        elp[name] = value
+def test_expect_log_joint_linear_model() -> None:
+    n = 7
+    p = 2
+    rng = ifnt.random.JaxRandomState(8)
+    data = {
+        "X": rng.normal((n, p)),
+        "y": rng.normal((n,)),
+    }
+    conditioned = numpyro.handlers.condition(linear_model, data)
+    approximation = {
+        "tau": distributions.Gamma(*(rng.gamma(10, (2,)) / 10)),
+        "coef": PrecisionNormal(rng.normal((p,)), rng.gamma(7, (p,)) / 7),
+        "intercept": PrecisionNormal(rng.normal(), rng.gamma(11) / 9),
+    }
+    elp = avb.expect_log_joint(conditioned, approximation, aggregate=False)(
+        n, p, delay=True
+    )
 
     # Verify numerically. This can probably be done more efficiently by batching. But
     # it's just a test.
@@ -87,10 +76,10 @@ def test_smoke() -> None:
                 if isinstance(value, distributions.Distribution)
                 else value
             )
-            for key, value in substitutions.items()
+            for key, value in approximation.items()
         }
         with numpyro.handlers.trace() as trace, numpyro.handlers.condition(data=values):
-            model(False, n, p)
+            conditioned(n, p, delay=False)
 
         for name, site in trace.items():
             log_densities.setdefault(name, []).append(
@@ -103,3 +92,39 @@ def test_smoke() -> None:
         log_densities,
         elp,
     )
+
+
+def test_elbo_linear_model() -> None:
+    n = 7
+    p = 2
+    rng = ifnt.random.JaxRandomState(8)
+    data = {
+        "X": rng.normal((n, p)),
+        "y": rng.normal((n,)),
+    }
+    conditioned = numpyro.handlers.condition(linear_model, data)
+    approximation = {
+        "tau": distributions.Gamma(*(rng.gamma(10, (2,)) / 10)),
+        "coef": PrecisionNormal(rng.normal((p,)), rng.gamma(7, (p,)) / 7),
+        "intercept": PrecisionNormal(rng.normal(), rng.gamma(11) / 9),
+    }
+    elbo = avb.elbo_loss(conditioned, approximation)(n, p, delay=True)
+
+    def guide(*args, **kwargs):
+        return {
+            name: numpyro.sample(name, dist) for name, dist in approximation.items()
+        }
+
+    trace_elbo = numpyro.infer.Trace_ELBO()
+    n_samples = 1000
+    trace_elbos = []
+    for _ in range(n_samples):
+        params = numpyro.handlers.seed(guide, rng.get_key())()
+        trace_elbos.append(
+            trace_elbo.loss(
+                rng.get_key(), params, conditioned, guide, n, p, delay=False
+            )
+        )
+    trace_elbos = jnp.stack(trace_elbos)
+
+    ifnt.testing.assert_samples_close(trace_elbos, elbo)
