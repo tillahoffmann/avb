b325dcea8e859c28b41554508fe5844dfdeb2d27
Till Hoffmann <tillahoffmann@gmail.com>
2024-05-01 23:33:50 -0400

    Use a `delay` messenger to handle log expected probability evaluating.

--- avb/__init__.py
+++ avb/__init__.py
@@ -1,11 +1,12 @@
 from ._expect import expect
 from ._expect_log_prob import expect_log_prob
 from . import distributions
-from .nodes import LazyDistribution
+from .nodes import delay, DelayedDistribution
 
 __all__ = [
+    "delay",
+    "DelayedDistribution",
     "distributions",
     "expect",
     "expect_log_prob",
-    "LazyDistribution",
 ]
