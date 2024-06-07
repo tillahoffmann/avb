733a34409ab3ad6a761e74a38f1cc2bbcff0bcf3
Till Hoffmann <tillahoffmann@gmail.com>
2024-06-07 15:03:15 -0400

    Add `hessdiag` utility function.

--- avb/util.py
+++ avb/util.py
@@ -87,3 +87,73 @@ def hessian_diagonal_finite_diff(func, x, *args, eps=1e-6, **kwargs):
     grad1 = grad(jax.tree.map(lambda x: x - eps, x), *args, **kwargs)
     grad2 = grad(jax.tree.map(lambda x: x + eps, x), *args, **kwargs)
     return jax.tree.map(lambda x, y: (y - x) / (2 * eps), grad1, grad2)
+
+
+def normalize_path(path):
+    """
+    Normalize keys returned by `jax.tree_util.tree_flatten_with_path`.
+    """
+    return tuple(
+        (
+            key.key
+            if isinstance(key, (jax.tree_util.DictKey, jax.tree_util.SequenceKey))
+            else key
+        )
+        for key in path
+    )
+
+
+def tree_set(tree, path, value, strict=False):
+    """
+    Set the `value` at `path` in `tree`. Raise a `KeyError` if the path does not exist
+    and `strict`.
+    """
+    path = normalize_path(path)
+    items, treedef = jax.tree_util.tree_flatten_with_path(tree)
+    values = []
+    replaced = False
+    for keys, original_value in items:
+        if normalize_path(keys) == path:
+            values.append(value)
+            replaced = True
+        else:
+            values.append(original_value)
+    if strict and not replaced:
+        raise KeyError(path)
+    return jax.tree.unflatten(treedef, values)
+
+
+def tree_get(tree, path):
+    """
+    Get the value at `path` in `tree`. Raise an error if the path does not exist.
+    """
+    for key in normalize_path(path):
+        tree = tree[key]
+    return tree
+
+
+def hessdiag(func):
+    """
+    Compute diagonal elements of the Hessian. Should be `jax.jit`ed to avoid excessive
+    memory use.
+    """
+
+    def _hessdiag_wrapper(x, *args, **kwargs):
+
+        def _leaf_hessdiag(path, value):
+            """
+            Compute the diagonal Hessian for `path` in `x`.
+            """
+            hessian = jax.jacfwd(
+                jax.jacrev(lambda y: func(tree_set(x, path, y), *args, **kwargs))
+            )(value)
+            assert value.size**2 == hessian.size
+            # Ravel, extract diagonal, reshape to the target shape.
+            return jnp.diagonal(hessian.reshape((value.size, value.size))).reshape(
+                value.shape
+            )
+
+        # Map the diagonal Hessian helper function over all leaves of the pytree.
+        return jax.tree_util.tree_map_with_path(_leaf_hessdiag, x)
+
+    return _hessdiag_wrapper
--- tests/test_util.py
+++ tests/test_util.py
@@ -1,4 +1,7 @@
 from avb import util
+import ifnt
+import jax
+from jax import numpy as jnp
 import numpy as np
 
 
@@ -16,4 +19,37 @@ def test_precondition_diagonal():
 def test_hessian_diagonal_finite_diff():
     np.testing.assert_allclose(
         util.hessian_diagonal_finite_diff(lambda x, a: a * x**2, 1, 3, eps=1e-8), 6
+    )
+
+
+def test_hessdiag():
+    rng = ifnt.random.JaxRandomState(17)
+    arg = {
+        "params": {
+            "loc": rng.normal((30,)),
+            "scale": rng.gamma(3, (30,)),
+        },
+        "x": rng.normal((30,)),
+    }
+
+    def func(arg):
+        z = (arg["x"] - arg["params"]["loc"]) / arg["params"]["scale"]
+        return jnp.square(z).sum()
+
+    hessdiag = jax.jit(util.hessdiag(func))(arg)
+    hess = jax.jacfwd(jax.jacrev(func))(arg)
+
+    # Compare the results.
+    for path, value in jax.tree_util.tree_flatten_with_path(hessdiag)[0]:
+        # Get the corresponding full Hessian and extract diagonal elements.
+        reference = util.tree_get(hess, path * 2)
+        reference = jnp.diagonal(reference.reshape((value.size, value.size))).reshape(
+            value.shape
+        )
+        np.testing.assert_allclose(value, reference, rtol=1e-6)
+
+    # Sanity checks.
+    np.testing.assert_allclose(hessdiag["x"], hessdiag["params"]["loc"], rtol=1e-6)
+    np.testing.assert_allclose(
+        hessdiag["x"], 2 / arg["params"]["scale"] ** 2, rtol=1e-6
     )
